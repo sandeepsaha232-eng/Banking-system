db53271dac18688b7a3cb3e824e8ba902d142710
nitishsingh10 <nitishsingh9hfhs@gmail.com>
2026-06-21 21:24:49 +0530

    font color fix

--- Frontend/js/pages/dashboard.js
+++ Frontend/js/pages/dashboard.js
@@ -12,7 +12,7 @@ document.getElementById('checkBalanceBtn').addEventListener('click', async () =>
     setTimeout(()=>{
         if(balance && balance.success){ 
             balanceText.textContent = 'your current balance : π' + balance.data.balance;
-            balanceText.style.color = 'var(--text-light)';
+            balanceText.style.color = 'aliceblue';
         }
         else{
             balanceText.textContent = 'could not fetch the balance :' + balance.data.message;
